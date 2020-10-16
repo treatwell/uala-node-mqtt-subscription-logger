a30943d77958bb0aa4cbfcbc01dc300cc5225aa7
Luca Mattivi <luca@mattivi.io>
2020-10-16 20:56:38 +0200

    Add error log on subiscribe

--- main.js
+++ main.js
@@ -15,7 +15,18 @@ client.on('error', function(error) {
   console.log('Can\'t connect' + error);
   process.exit(1);
 });
+
 client.on('message', function(topic, message, packet) {
   console.log('Received message in topic ' + topic + ' - ' + message);
 });
-client.subscribe(process.env.MQTT_TOPIC, { qos: 1 });
+
+client.subscribe(process.env.MQTT_TOPIC, { qos: 0 }, function (err) {
+  if (!err) {
+    console.log('Subscribed to topic ' + process.env.MQTT_TOPIC);
+  }
+  else{
+    console.log('Cannot subscribe to ' + process.env.MQTT_TOPIC);
+    console.log(err);
+    process.exit(1);
+  }
+});
